a183ec14ae9bd47f3e79375a86207f39ebb5b031
Sergey Marunich <marunich.s@gmail.com>
2020-02-24 21:51:47 -0500

    K8S cloud discover and config

--- lab_outputs.tf
+++ lab_outputs.tf
@@ -5,6 +5,10 @@ output "Jumpbox_PublicIP" {
   value = aws_instance.jumpbox.public_ip
 }
 
+output "Master_PublicIP" {
+  value = aws_instance.master.*.public_ip
+}
+
 output "Controller_PublicIP" {
   value = aws_eip.ctrl_eip.*.public_ip
 }

17f8250ce97ddf67f639c1d082c1d865d9de92d0
Sergey Marunich <marunich.s@gmail.com>
2020-02-25 14:12:36 -0500

    adding ingress via avi (kuard, kubernetes-dashboard)

--- aws_security_groups.tf
+++ aws_security_groups.tf
@@ -102,7 +102,7 @@ resource "aws_security_group" "jumpbox_sg" {
 }
 
 resource "aws_security_group" "k8s_sg" {
-  description = "Allow incoming connections to the lab jumpbox."
+  description = "Allow incoming connections to the k8s nodes"
   vpc_id      = aws_vpc.K8S_vpc.id
   count       = var.student_count
 
@@ -114,6 +114,34 @@ resource "aws_security_group" "k8s_sg" {
   ingress {
     from_port   = 22
     to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 6443
+    to_port     = 6443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }

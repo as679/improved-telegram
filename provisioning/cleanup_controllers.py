65d575511a14674c77248bdfa91f60fd00f5ae49
as679 <as679@users.noreply.github.com>
2020-03-30 10:43:31 +0100

    Updated cleanup_controllers.py for Python3

--- provisioning/cleanup_controllers.py
+++ provisioning/cleanup_controllers.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 import sys
 import redis
 import threading
@@ -12,24 +12,24 @@ def get_admin_password(r, tag='Lab_avi_admin_password'):
             return r.hget(name, tag)
 
 def clean_up(ctrl, passwd):
-  print "Cleaning Controller IP: %s" % ctrl
+  print("Cleaning Controller IP: %s" % ctrl)
   no_access = { 'vtype': 'CLOUD_NONE' }
   api = ApiSession.get_session(ctrl, "admin", passwd, tenant="admin", timeout=600)
   for i in api.get('virtualservice').json()['results']:
-      print "%s:Deleting VS: %s" % (ctrl, i['name'])
+      print("%s:Deleting VS: %s" % (ctrl, i['name']))
       api.delete_by_name('virtualservice', i['name'])
   for i in api.get('serviceengine').json()['results']:
-      print "%s:Deleting SE: %s" % (ctrl, i['name'])
+      print("%s:Deleting SE: %s" % (ctrl, i['name']))
       api.delete_by_name('serviceengine', i['name'])
   for i in api.get('cloud').json()['results']:
-      print "%s:Forcing garbage collection on cloud: %s" % (ctrl, i['name'])
+      print("%s:Forcing garbage collection on cloud: %s" % (ctrl, i['name']))
       no_access['name'] = i['name']
       for j in range(5):
           api.put('cloud/%s/gc' % i['uuid'], params={'force': True})
-      print "%s:Setting NO_ACCESS on cloud: %s" % (ctrl, i['name'])
+      print("%s:Setting NO_ACCESS on cloud: %s" % (ctrl, i['name']))
       api.put('cloud/%s' % i['uuid'], data=no_access)
 
-r = redis.StrictRedis(host='localhost', port=6379, db=0)
+r = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
 threads = []
 ctrls = r.sscan('controllers')[1]
 
